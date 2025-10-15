1a15c5000381046842729111415d7724aa8ebaa5
lucasiefp <geral@kubisseya.pt>
2025-10-15 04:44:54 -0500

    Funcao para quartos

--- quartos.py
+++ quartos.py
@@ -1,2 +1,11 @@
+rooms = [
+    {"number": 101, "tipo": "Solteiro", "disponivel": True},
+    {"number": 102, "tipo": "Casal", "disponivel": False}
+]
+
 def listar_quartos():
-    print("Lista de quartos disponíveis (teste).")
+    print("\n=== Lista de Quartos ===")
+    for q in rooms:
+        estado = "Disponível" if q["disponivel"] else "Ocupado"
+        print(f"Quarto {q['number']} - {q['tipo']} ({estado})")
+
